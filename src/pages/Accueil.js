8b3cb6236bb89011df7c056f76a7a55e826eefbf
Anthony <anthonyoutub@gmail.com>
2023-11-07 11:24:15 +0100

    new component card

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import CardCocktail from '../components/CardCocktail';
 
 const Accueil = () => {
     return (
         <div>
             <h1>Accueil</h1>
+            <CardCocktail
+                title="Titre de la carte"
+                text="Texte de la carte"
+                img="URL de l'image"
+            />
         </div>
     );
 };
