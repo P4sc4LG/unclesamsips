c6889592ff96c5518e7a49de9043cec5132b30b0
Anthony <anthonyoutub@gmail.com>
2023-11-11 10:11:07 +0100

    Refactor import of css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
+import './header.css';
+
 import logo from '../../assets/logo.png';
+import UserIcon from '../../assets/icons/user_icon.png';
+import { Label, Toggle } from '../'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import Label from '../Label/Label';
-import Toggle from '../Toggle/Toggle';
-import UserIcon from '../../assets/icons/user_icon.png';
-
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-white" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
       <Container>
-        {/* TODO transformer en component */}
-        <img style={{ width: 250,marginRight: '5em'}} src={logo} alt="logo" />
+        <Link to='/'><img style={{ width: 250,marginRight: '5em'}} src={logo} alt="logo" /></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to='/' className="text-decoration-none col px-md-3"><Nav.Item><Label content={"Accueil"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
-            <Link to='/exemple' className="text-decoration-none col px-md-3" ><Nav.Item><Label content={"Ingrédient"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
-            <Link to='/exemple' className="text-decoration-none col px-md-3" ><Nav.Item><Label content={"Cock'Random"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
-            <Link to='/Accueil' className="text-decoration-none col px-md-3" ><Nav.Item><Label content={"Autre1"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
+            <Link to='/' className="text-decoration-none col px-md-3 yellow-police"><Nav.Item><Label content={"Accueil"} /></Nav.Item></Link>
+            <Link to='/exemple' className="text-decoration-none col px-md-3 yellow-police" ><Nav.Item><Label content={"Ingrédient"} /></Nav.Item></Link>
+            <Link to='/exemple' className="text-decoration-none col px-md-3 yellow-police" ><Nav.Item><Label content={"Cock'Random"} /></Nav.Item></Link>
+            <Link to='/Accueil' className="text-decoration-none col px-md-3 yellow-police" ><Nav.Item><Label content={"Autre1"} /></Nav.Item></Link>
             
-            <Link to='/exemple' className="text-decoration-none col px-md-3" ><Nav.Item><Label content={"Autre2"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
-            <Link to='/exemple' className="text-decoration-none col px-md-3" ><Nav.Item><Label content={"Autre3"} color={"rgba(252, 249, 69, 0.8)"}/></Nav.Item></Link>
+            <Link to='/exemple' className="text-decoration-none col px-md-3 yellow-police" ><Nav.Item><Label content={"Autre2"} /></Nav.Item></Link>
+            <Link to='/exemple' className="text-decoration-none col px-md-3 yellow-police" ><Nav.Item><Label content={"Autre3"} /></Nav.Item></Link>
           </Nav>
           <Nav className="ms-auto">
               <Nav.Item style={{ paddingTop:'0.3em'}}><Toggle/></Nav.Item>
