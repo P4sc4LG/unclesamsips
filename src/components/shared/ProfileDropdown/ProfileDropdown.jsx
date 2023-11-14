2c10b5b8b21b92af40c8f4a786c4b192991e2e9c
Anthony <anthonyoutub@gmail.com>
2023-11-14 11:08:38 +0100

    dropdown login or not

--- src/components/shared/ProfileDropdown/ProfileDropdown.jsx
+++ src/components/shared/ProfileDropdown/ProfileDropdown.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import { accountService } from '@/_services/account.service';
+import { useNavigate } from 'react-router-dom';
 
 import UserIcon from '@/assets/icons/user_icon.png';
 
 const ProfileDropdown = () => {
+
+    let navigate = useNavigate();
+
+    const logout = () => {
+        accountService.logout()
+        navigate('/')
+    }
+
     return (
         <NavDropdown title={<img src={UserIcon} alt="User" style={{ width: 30, height: 30 }} />} id="nav-dropdown" drop="down">
-            <NavDropdown.Item as={Link} to="/admin/dashboard">
-                Administration
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/admin/dashboard">
-                Se déconnecter
-            </NavDropdown.Item>
+            {accountService.isLogged() ? (
+                <>
+                    <NavDropdown.Item as={Link} to="/admin/dashboard">
+                        Administration
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logout}>
+                        Se déconnecter
+                    </NavDropdown.Item>
+                </>
+            ) : (
+                <NavDropdown.Item as={Link} to="/admin/dashboard">
+                    Se connecter
+                </NavDropdown.Item>
+            )}
         </NavDropdown>
     );
 };
