f9432ab1514a46c124e70648d2f50156ee6f03f8
pascal <dupascal00@gmail.com>
2023-11-15 09:45:30 +0100

    add config in local

--- src/components/shared/Header/Header.stories.js
+++ src/components/shared/Header/Header.stories.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from './Header';
-
+import { DarkModeProvider } from '../../../context/DarkModeContext';
+import { MemoryRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default {
-    title: 'Shared/Header',
-    component: Header,
-  };
+  title: 'Shared/Header',
+  component: Header,
+  decorators: [
+    (Story) => (
+      <DarkModeProvider>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </DarkModeProvider>
+    ),
+  ],
+};
 
 const Template = () => <Header />;
 
